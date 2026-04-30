be095428044f683c5e1b39d438f6639083602eda
Prangya Priyadarshini <prangyapriyadarshini2004@gmail.com>
2026-04-30 23:01:34 +0530

    react

---
+++ python niis/react program/my-create-app/src/App1.jsx
@@ -0,0 +1,14 @@
+function App1() {
+	// Creating JSX
+	const element= <h1>Hello JSX!</h1>
+
+	return (
+		<div>
+
+			{element}
+			
+			<p>This is a paragraph</p>
+		</div>
+	)
+}
+export default App1
---
+++ python niis/react program/myapp/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+function App() {
+  return (
+    <div>
+      <h1>Hello React</h1>
+      <p>This is my first React app</p>
+    </div>
+  );
+}
+
+export default App;
